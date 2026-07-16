96342338af24f40b43af77def38711a5015b734d
nikolaenko <aleshka.dev@gmail.com>
2026-07-16 14:11:47 +0300

    feat(ui): add Separator component and update layout with SidebarInset

--- app/team/[teamId]/layout.tsx
+++ app/team/[teamId]/layout.tsx
@@ -7,7 +7,9 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarInset,
   SidebarProvider,
+  SidebarTrigger,
 } from "@/src/shared/ui"
 import { NavProjects } from "./NavProjects"
 import { TeamSwitcher } from "./TeamPicker"
@@ -30,7 +32,7 @@ export default async function TeamLayout({
   return (
     <>
       <SidebarProvider>
-        <Sidebar>
+        <Sidebar collapsible="icon">
           <SidebarHeader className="border-b">
             <TeamSwitcher teams={teams} />
           </SidebarHeader>
@@ -43,6 +45,22 @@ export default async function TeamLayout({
             {isAuth ? <UserNav user={user} /> : <AuthButton />}
           </SidebarFooter>
         </Sidebar>
+
+        <SidebarInset className="">
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b! transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex h-16 shrink-0 items-center gap-2 px-4">
+              <SidebarTrigger className="" />
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+              <div className="aspect-video rounded-xl bg-muted/50" />
+              <div className="aspect-video rounded-xl bg-muted/50" />
+              <div className="aspect-video rounded-xl bg-muted/50" />
+            </div>
+            <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          </div>
+        </SidebarInset>
       </SidebarProvider>
     </>
   )
