1681bc5a4d2301be27d8ec29c3d997fe209d5bd3
nikolaenko <aleshka.dev@gmail.com>
2026-07-24 19:28:36 +0300

    feat(sign-out): simplify sign-out action by using api.post and removing redundant fetch logic

--- src/entity/user/api/sign-out.action.ts
+++ src/entity/user/api/sign-out.action.ts
@@ -1,23 +1,14 @@
 "use server"
 
+import { api } from "@/src/shared/api"
 import { API_URL } from "@/src/shared/constants"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export async function signOutAction() {
   const cookieStore = await cookies()
-  const accessToken = cookieStore.get("accessToken")?.value
 
-  try {
-    await fetch(`${API_URL}/auth/logout`, {
-      method: "POST",
-      headers: {
-        Cookie: `accessToken=${accessToken}`,
-      },
-    })
-  } catch (error) {
-    console.error("Logout API error:", error)
-  }
+  await api.post(`${API_URL}/auth/logout`)
 
   cookieStore.delete("accessToken")
   cookieStore.delete("refreshToken")
